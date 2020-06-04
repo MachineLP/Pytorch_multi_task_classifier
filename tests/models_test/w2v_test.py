03731fc10213e0b126e90bc545baf5f0ad15b855
MachineLP <MachineLP@163.com>
2020-06-04 23:21:22 +0800

    添加w2v训练

--- tests/models_test/w2v_test.py
+++ tests/models_test/w2v_test.py
@@ -18,7 +18,7 @@ from textmatch.config.constant import Constant as const
 if __name__ == '__main__':
     words_list = ["我去玉龙雪山并且喜欢玉龙雪山玉龙雪山","我在玉龙雪山并且喜欢玉龙雪山","我在九寨沟"]
     w2v = Word2Vec(w2v_model_file=const.W2V_MODEL_FILE,  stop_word=StopWords(stopwords_file=const.STOPWORDS_FILE) )
-    w2v.init(words_list, , update=True)
+    w2v.init(words_list, update=True)
     testword = "我在九寨沟,很喜欢"
     pre = w2v.predict(testword)
     print ('pre>>>>>', pre) 
