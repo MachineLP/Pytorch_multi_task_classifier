3456e1664e486549499a48c13d5bf7722f888693
MachineLP <MachineLP@163.com>
2020-06-15 14:08:14 +0800

    添加降维pca、检索faiss

---
+++ textmatch/tools/decomposition/pca.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+'''
+-------------------------------------------------
+   Description :  PCA
+   Author :       machinelp
+   Date :         2020-06-15
+-------------------------------------------------
+
+'''
+
+import numpy as np
+from sklearn.decomposition import PCA
+
+
+class PCA(object):
+    def __init__(self, ):
+        pass
+
+
