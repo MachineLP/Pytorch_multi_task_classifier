25dcaec8e9d287ea92856300f8dc83648b68d368
liupeng <liupeng@qudian.com>
2020-06-16 22:23:55 +0800

    优化ml

--- textmatch/models/ml/gbd_lr.py
+++ textmatch/models/ml/gbd_lr.py
@@ -16,15 +16,19 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.linear_model import LinearRegression
 
 class GBDTLR:
-
     def __init__(self):
+        self.other_params = {'learning_rate': cfg.lr.learning_rate
+                             }
+        self.clf = (...)
         pass
 
-    def fit(self):
-        pass
+    def fit(self, train_x, train_y):
+        self.clf.fit(train_x, train_y)
+        return self
 
-    def predict(self):
-        pass
+    def predict(self, X_test):
+        predict = self.model.predict_proba(X_test)[:,1]
+        return predict
 
     def save_model(self):
         pass
--- textmatch/models/ml/lgb.py
+++ textmatch/models/ml/lgb.py
@@ -15,15 +15,19 @@ from textmatch.config.constant import Constant as const
 import lightgbm as lgb
 
 class LGB:
-
     def __init__(self):
+        self.other_params = {'learning_rate': cfg.lr.learning_rate
+                             }
+        self.clf = (...)
         pass
 
-    def fit(self):
-        pass
+    def fit(self, train_x, train_y):
+        self.clf.fit(train_x, train_y)
+        return self
 
-    def predict(self):
-        pass
+    def predict(self, X_test):
+        predict = self.model.predict_proba(X_test)[:,1]
+        return predict
 
     def save_model(self):
         pass
