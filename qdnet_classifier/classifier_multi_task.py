1603be404d0150bc4c547a09c06c8dcb83b6a974
MachineLP <MachineLP@163.com>
2022-05-20 14:10:54 +0400

    Update classifier_multi_task.py

--- qdnet_classifier/classifier_multi_task.py
+++ qdnet_classifier/classifier_multi_task.py
@@ -106,6 +106,72 @@ class MultiLabelModel(nn.Module):
 
         return metrics
 
+        @staticmethod
+    def print_confusion_matrix(
+        y_true: np.ndarray,
+        y_pred: np.ndarray,
+        labels: Optional[List] = None,
+        hide_zeroes: bool = False,
+        hide_diagonal: bool = False,
+        hide_threshold: Optional[float] = None,
+        class_names: Optional[List[str]] = None,
+    ):
+        """Print a nicely formatted confusion matrix with labelled rows and columns.
+
+        Predicted labels are in the top horizontal header, true labels on the vertical header.
+
+        Args:
+            y_true (np.ndarray): ground truth labels
+            y_pred (np.ndarray): predicted labels
+            labels (Optional[List], optional): list of all labels. If None, then all labels present in the data are
+                displayed. Defaults to None.
+            hide_zeroes (bool, optional): replace zero-values with an empty cell. Defaults to False.
+            hide_diagonal (bool, optional): replace true positives (diagonal) with empty cells. Defaults to False.
+            hide_threshold (Optional[float], optional): replace values below this threshold with empty cells. Set to None
+                to display all values. Defaults to None.
+        """
+        if labels is None:
+            if class_names is None:
+                labels = np.unique(np.concatenate((y_true, y_pred)))
+            else:
+                labels = np.arange(len(class_names))
+
+        cm = confusion_matrix(y_true, y_pred, labels=labels)
+
+        if class_names is not None:
+            labels = class_names
+
+        # find which fixed column width will be used for the matrix
+        columnwidth = max(
+            [len(str(x)) for x in labels] + [5]
+        )  # 5 is the minimum column width, otherwise the longest class name
+        empty_cell = ' ' * columnwidth
+
+        # top-left cell of the table that indicates that top headers are predicted classes, left headers are true classes
+        padding_fst_cell = (columnwidth - 3) // 2  # double-slash is int division
+        fst_empty_cell = padding_fst_cell * ' ' + 't/p' + ' ' * (columnwidth - padding_fst_cell - 3)
+
+        # Print header
+        print('    ' + fst_empty_cell, end=' ')
+        for label in labels:
+            print(f'{label:{columnwidth}}', end=' ')  # right-aligned label padded with spaces to columnwidth
+
+        print()  # newline
+        # Print rows
+        for i, label in enumerate(labels):
+            print(f'    {label:{columnwidth}}', end=' ')  # right-aligned label padded with spaces to columnwidth
+            for j in range(len(labels)):
+                # cell value padded to columnwidth with spaces and displayed with 1 decimal
+                cell = f'{cm[i, j]:{columnwidth}}'
+                if hide_zeroes:
+                    cell = cell if float(cm[i, j]) != 0 else empty_cell
+                if hide_diagonal:
+                    cell = cell if i != j else empty_cell
+                if hide_threshold:
+                    cell = cell if cm[i, j] > hide_threshold else empty_cell
+                print(cell, end=' ')
+            print()
+
 
 class MultilabelClassifier:
     def __init__(self, device, classifier_model, weights, num_color_classes, num_action_classes, use_ema=False):
