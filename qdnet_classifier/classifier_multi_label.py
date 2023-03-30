efa59b54015b8a108c98238e6a9082d5e9ebc76a
peng liu <peng.liu@matrx.team>
2023-03-30 11:55:30 +0400

    Update

--- qdnet_classifier/classifier_multi_label.py
+++ qdnet_classifier/classifier_multi_label.py
@@ -15,6 +15,7 @@ import warnings
 import numpy as np
 from typing import Optional
 from typing import List
+from mtcnn import MTCNN
 
 
 
@@ -67,7 +68,7 @@ class MultiLabelModel(nn.Module):
         super(MultiLabelModel, self).__init__()
 
         self.model_classifier = create_model( classifier, pretrained=pretrained, )
-        last_channel = 512
+        last_channel = 512  # 512, 1024, 2048
         self.pool = nn.AdaptiveAvgPool2d((1, 1))
         self.output = nn.Sequential(
                      nn.Dropout(p=0.5),
